a76a481f68080173973721281ab7d1f0f28e1c1e
A W <amwtablet@gmail.com>
2021-10-01 09:37:36 -0600

    added constants for fiat type/amount and 1 crypto type/amount, as well as rate & submit consts

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Java
+
+
+//Upfront Definitions
+const fiatType = document.getElementById('fiatType-ID');
+const crypto1Type = document.getElementById('crypto1Type-ID');
+const fiatAmount = document.getElementById('fiatAmount-ID');
+const crypto1Amount = document.getElementById('amount-two');
+
+const rate = document.getElementById('rate');
+const submit = document.getElementById('submit');
