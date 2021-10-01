fd810779998db760f23ed4261f7b74cd108135a2
A W <amwtablet@gmail.com>
2021-10-01 10:02:13 -0600

    added event listeners in js, linked js IDs in HTML, added fiat selection options

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ const crypto1Amount = document.getElementById('amount-two');
 
 const rate = document.getElementById('rate');
 const submit = document.getElementById('submit');
+
+// Event Listeners
+fiatType.addEventListener('change', calculate);
+fiatAmount.addEventListener('input', calculate);
+crypto1Type.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+submit.addEventListener('click', () => {
+  const temp = fiatType.value;
+  fiatType.value = crypto1Type.value;
+  crypto1Type.value = temp;
+  calculate();
+});
+
+
