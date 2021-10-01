7fd9b1cbd6cc1ff3f5914585e6fa69207fbf9d3e
Tari <bota.maral@gmail.com>
2021-10-01 19:33:53 -0400

    fetch api

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Java
+   fetch(' https://api.coinlore.net/api/tickers/?start=200&limit=100')
+  .then(response => response.json())
+  .then(data => console.log(data));
+//   .then (function(response) {
+//     var coin = document.querySelector(".coin");
+//     var crypto = document.createElement("li");
+//     crypto.setAttribute("class", response.data[0]);
+//     coin.appendChild(crypto);
+//   })
+
+  
