23fd57f49247aae352feb7c5fdc296f1bf3c1d67
Tari <bota.maral@gmail.com>
2021-10-01 23:23:40 -0400

    load data

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-   fetch(' https://api.coinlore.net/api/tickers/?start=200&limit=100')
+   fetch(' https://api.coinlore.net/api/tickers/')
   .then(response => response.json())
-  .then(data => console.log(data));
-//   .then (function(response) {
-//     var coin = document.querySelector(".coin");
-//     var crypto = document.createElement("li");
-//     crypto.setAttribute("class", response.data[0]);
-//     coin.appendChild(crypto);
-//   })
+  .then(coins => showCrypto(coins.data));
+
+  const showCrypto = coins => {
+    const coinList = document.querySelector(".coin-list");
+    const coinPrice = document.querySelector(".price");
+    const coinMcap =document.querySelector(".market-cap");
+    const coinOneHour = document.querySelector(".one-hour");
+    const coinHours = document.querySelector(".hours");
+
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      coinEl.innerHTML = (coin.symbol + " - " + coin.name);
+      coinList.appendChild(coinEl);
+    })
+
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      coinEl.innerHTML = (coin.price_usd);
+      coinPrice.appendChild(coinEl);
+    })
+
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      coinEl.innerHTML = (coin.market_cap_usd);
+      coinMcap.appendChild(coinEl);
+    })
+
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      coinEl.innerHTML = (coin.percent_change_1h);
+     coinOneHour.appendChild(coinEl);
+    })
+
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      coinEl.innerHTML = (coin.percent_change_24h);
+      coinHours.appendChild(coinEl);
+    })
+  }
+
 
   
