bd9c6bb563f7062b3baa195656fd3092bc4ceffb
Alabama Whiskey <amwtablet@gmail.com>
2021-10-04 16:59:01 -0400

    added converter framework in js & if statements for currencies selected, added cosmetic details to api info such as marketcap rounding and dollar and percent symbols

--- script.js
+++ script.js
@@ -8,6 +8,7 @@
     const coinMcap =document.querySelector(".market-cap");
     const coinOneHour = document.querySelector(".one-hour");
     const coinHours = document.querySelector(".hours");
+    const coinOutput = document.querySelector(".output")
 
     coins.forEach(coin => {
       const coinEl = document.createElement("p");
@@ -17,28 +18,49 @@
 
     coins.forEach(coin => {
       const coinEl = document.createElement("p");
-      coinEl.innerHTML = (coin.price_usd);
+      coinEl.innerHTML = ("$" + coin.price_usd);
       coinPrice.appendChild(coinEl);
     })
 
     coins.forEach(coin => {
       const coinEl = document.createElement("p");
-      coinEl.innerHTML = (coin.market_cap_usd);
+      //converts result into billions, rounding to 2 decimals
+      coinEl.innerHTML = ("$" + Math.round((coin.market_cap_usd / 1000000000)*100)/100 + " Billion");
       coinMcap.appendChild(coinEl);
     })
 
     coins.forEach(coin => {
       const coinEl = document.createElement("p");
-      coinEl.innerHTML = (coin.percent_change_1h);
+      coinEl.innerHTML = (coin.percent_change_1h + "%");
      coinOneHour.appendChild(coinEl);
     })
 
     coins.forEach(coin => {
       const coinEl = document.createElement("p");
-      coinEl.innerHTML = (coin.percent_change_24h);
+      coinEl.innerHTML = (coin.percent_change_24h + "%");
       coinHours.appendChild(coinEl);
     })
-  }
+
+    //Output Calculator (framework - needs work)
+    coins.forEach(coin => {
+      const coinEl = document.createElement("p");
+      if (document.getElementById("rate") = "USD") {
+        coinEl.innerHTML = (coin.price_usd / document.getElementById("amount"));
+      };
+
+      if (document.getElementById("rate") = "GBP") {
+        coinEl.innerHTML = ((coin.price_usd*.73) / document.getElementById("amount"));
+      };
+
+      if (document.getElementById("rate") = "EUR") {
+        coinEl.innerHTML = ((coin.price_usd*.86) / document.getElementById("amount"));
+      };
+
+      coinOutput.appendChild(coinEl);
+    })
+  };  
+
+
 
 
   
